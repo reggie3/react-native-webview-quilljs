e7dcc1d5a9a63139b0ebd00bb32efe55b754147f
Reginald Johnson <reginald.johnson@gmail.com>
2018-02-11 15:31:07 -0500

    update readme

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // name and version of the package that contains the index file(s) the webview will load
 export const PACKAGE_NAME = 'react-native-webview-quilljs';
-export const PACKAGE_VERSION = '0.1.33';
+export const PACKAGE_VERSION = '0.1.34';
 
 // use the local html files from the dist folder instead of using the downloaded ones
 // used for debugging
-export const USE_LOCAL_FILES = true;
+export const USE_LOCAL_FILES = false;
