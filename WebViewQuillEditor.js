3b1fea15e0201d2ab60536cbbfe63af86605ee0a
Reginald Johnson <reginald.johnson@gmail.com>
2018-03-03 22:50:01 -0500

    "publishing"

--- WebViewQuillEditor.js
+++ WebViewQuillEditor.js
@@ -39,126 +39,6 @@ const reactHtml = require('./assets/dist/reactQuillEditor-index.html');
 const MESSAGE_PREFIX = 'react-native-webview-quilljs';
 
 export default class WebViewQuillEditor extends React.Component {
-<<<<<<< HEAD
-	constructor() {
-		super();
-		this.state = {
-			webViewNotLoaded: true, // flag to show activity indicator
-			webViewFilesNotAvailable: true
-		};
-	}
-
-	createWebViewRef = (webview) => {
-		this.webview = webview;
-	};
-
-	handleMessage = (event) => {
-		let msgData;
-		try {
-			msgData = JSON.parse(event.nativeEvent.data);
-			if (msgData.hasOwnProperty('prefix') && msgData.prefix === MESSAGE_PREFIX) {
-				// console.log(`WebViewQuillEditor: received message ${msgData.type}`);
-				this.sendMessage('MESSAGE_ACKNOWLEDGED');
-				// console.log(`WebViewQuillEditor: sent MESSAGE_ACKNOWLEDGED`);
-
-				switch (msgData.type) {
-					case 'EDITOR_MOUNTED':
-						this.setState({ webViewNotLoaded: false });
-						break;
-					case 'TEXT_CHANGED':
-						if (this.props.onDeltaChangeCallback) this.props.onDeltaChangeCallback(msgData.payload.delta);
-						break;
-					case 'RECEIVE_DELTA':
-						if (this.props.getDeltaCallback) this.props.getDeltaCallback(msgData.payload);
-						break;
-					default:
-						console.warn(`WebViewQuillEditor Error: Unhandled message type received "${msgData.type}"`);
-				}
-			}
-		} catch (err) {
-			console.warn(err);
-			return;
-		}
-	};
-
-	webViewLoaded = () => {
-		console.log('Webview loaded');
-		this.setState({ webViewNotLoaded: false });
-		// send the content to the editor if we have it
-		if (this.props.hasOwnProperty('contentToDisplay')) {
-			console.log(this.props.contentToDisplay);
-			this.sendMessage('SET_CONTENTS', {
-				delta: this.props.contentToDisplay
-			});
-		}
-	};
-
-	sendMessage = (type, payload) => {
-		// only send message when webview is loaded
-		if (this.webview) {
-			// console.log(`WebViewQuillEditor: sending message ${type}`);
-			this.webview.postMessage(
-				JSON.stringify({
-					prefix: MESSAGE_PREFIX,
-					type,
-					payload
-				}),
-				'*'
-			);
-		}
-	};
-
-	// get the contents of the editor.  The contents will be in the Delta format
-	// defined here: https://quilljs.com/docs/delta/
-	getDelta = () => {
-		this.sendMessage('GET_DELTA');
-	};
-
-	showLoadingIndicator = () => {
-		return (
-			<View style={styles.activityOverlayStyle}>
-				<View style={styles.activityIndicatorContainer}>
-					<ActivityIndicator size="large" animating={this.state.webViewNotLoaded} color="blue" />
-				</View>
-			</View>
-		);
-	};
-
-	onError = (error) => {
-		Alert.alert('WebView onError', error, [ { text: 'OK', onPress: () => console.log('OK Pressed') } ]);
-	};
-
-	renderError = (error) => {
-		Alert.alert('WebView renderError', error, [ { text: 'OK', onPress: () => console.log('OK Pressed') } ]);
-	};
-	render = () => {
-		return (
-			<View
-				style={{
-					flex: 1,
-					backgroundColor: '#ffebba'
-				}}
-			>
-				<WebView
-					style={{
-						...StyleSheet.absoluteFillObject,
-						backgroundColor: '#ffebba',
-						padding: 10
-					}}
-					ref={this.createWebViewRef}
-					source={require('./assets/dist/reactQuillEditor-index.html')}
-					onLoadEnd={this.webViewLoaded}
-					onMessage={this.handleMessage}
-					startInLoadingState={true}
-					renderLoading={this.showLoadingIndicator}
-					renderError={this.renderError}
-          onError={this.onError}
-          scalesPageToFit ={false}
-				/>
-			</View>
-		);
-	};
-=======
   constructor() {
     super();
     this.state = {
@@ -324,7 +204,6 @@ export default class WebViewQuillEditor extends React.Component {
       </View>
     );
   };
->>>>>>> parent of 486be4e... "publishing"
 }
 
 WebViewQuillEditor.propTypes = {
--- WebViewQuillViewer.js
+++ WebViewQuillViewer.js
@@ -6,138 +6,190 @@
  *
  */
 import React from 'react';
-import { View, ActivityIndicator, StyleSheet, WebView, Alert } from 'react-native';
+import { View, ActivityIndicator, StyleSheet, WebView } from 'react-native';
 import PropTypes from 'prop-types';
-const reactHtml = require('./assets/dist/reactQuillViewer-index.html');
+import renderIf from 'render-if';
+import * as webViewDownloadHelper from './webViewDownloadHelper';
+import { FileSystem } from 'expo';
+import config from './config';
+
+// path to the file that the webview will load
+const INDEX_FILE_PATH = `${FileSystem.documentDirectory}${
+  config.PACKAGE_NAME
+}/${config.PACKAGE_VERSION}/reactQuillViewer-index.html`;
+// the files that will be downloaded
+const FILES_TO_DOWNLOAD = [
+  'https://raw.githubusercontent.com/reggie3/react-native-webview-quilljs/master/dist/reactQuillViewer-index.html',
+  'https://raw.githubusercontent.com/reggie3/react-native-webview-quilljs/master/dist/viewer.bundle.js',
+  'https://raw.githubusercontent.com/reggie3/react-native-webview-quilljs/master/dist/reactQuillEditor-index.html',
+  'https://raw.githubusercontent.com/reggie3/react-native-webview-quilljs/master/dist/editor.bundle.js',
+  'https://raw.githubusercontent.com/reggie3/react-native-webview-quilljs/master/dist/common.js'
+];
 
 const MESSAGE_PREFIX = 'react-native-webview-quilljs';
 
 export default class WebViewQuillViewer extends React.Component {
-	constructor() {
-		super();
-		this.webview = null;
-		this.state = {
-			webViewNotLoaded: true, // flag to show activity indicator
-			webViewFilesNotAvailable: true
-		};
-	}
+  constructor() {
+    super();
+    this.webview = null;
+    this.state = {
+      webViewNotLoaded: true, // flag to show activity indicator
+      webViewFilesNotAvailable: true
+    };
+  }
 
-	sendContentToViewer = (delta) => {
-		debugger;
-		this.sendMessage('SET_CONTENTS', {
-			ops: delta.ops
-		});
-	};
+  componentDidMount = () => {
+    this.downloadWebViewFiles(FILES_TO_DOWNLOAD);
+  };
 
-	sendMessage = (type, payload) => {
-		// only send message when webview is loaded
-		if (this.webview) {
-			console.log(`WebViewQuillViewer: sending message ${type}`);
-			this.webview.postMessage(
-				JSON.stringify({
-					prefix: MESSAGE_PREFIX,
-					type,
-					payload
-				}),
-				'*'
-			);
-		}
-	};
+  downloadWebViewFiles = async filesToDownload => {
+    if (!config.USE_LOCAL_FILES) {
+      let downloadStatus = await webViewDownloadHelper.checkForFiles(
+        config.PACKAGE_NAME,
+        config.PACKAGE_VERSION,
+        filesToDownload,
+        this.webViewDownloadStatusCallBack
+      );
+      if (downloadStatus.success) {
+        this.setState({ webViewFilesNotAvailable: false });
+      } else if (!downloadStatus.success) {
+        console.log(
+          `unable to download html files: ${JSON.stringify(downloadStatus)}`
+        );
+        Alert.alert(
+          'Error',
+          `unable to download html files: ${JSON.stringify(downloadStatus)}`,
+          [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+          { cancelable: false }
+        );
+      } else {
+        this.setState({ webViewFilesNotAvailable: false });
+      }
+    } else {
+      this.setState({ webViewFilesNotAvailable: false });
+    }
+  };
 
-	webViewLoaded = () => {
-		this.setState({ webViewNotLoaded: false });
+  sendContentToViewer = delta => {
+    debugger;
+    this.sendMessage('SET_CONTENTS', {
+      ops: delta.ops
+    });
+  };
 
-		// send content to viewer if any was passed
-		if (this.props.hasOwnProperty('contentToDisplay')) {
-			console.log(this.props.contentToDisplay);
-			this.sendMessage('SET_CONTENTS', {
-				payload: {
-					ops: this.props.contentToDisplay.ops
-				}
-			});
-		}
-	};
+  sendMessage = (type, payload) => {
+    // only send message when webview is loaded
+    if (this.webview) {
+      console.log(`WebViewQuillViewer: sending message ${type}`);
+      this.webview.postMessage(
+        JSON.stringify({
+          prefix: MESSAGE_PREFIX,
+          type,
+          payload
+        }),
+        '*'
+      );
+    }
+  };
 
-	createWebViewRef = (webview) => {
-		this.webview = webview;
-	};
+  webViewLoaded = () => {
+    this.setState({ webViewNotLoaded: false });
 
-	handleMessage = (event) => {
-		console.log('WebViewQuillViewer handleMessage');
-	};
+    // send content to viewer if any was passed
+    if (this.props.hasOwnProperty('contentToDisplay')) {
+      console.log(this.props.contentToDisplay);
+      this.sendMessage('SET_CONTENTS', {
+        payload: {
+          ops: this.props.contentToDisplay.ops
+        }
+      });
+    }
+  };
 
-	showLoadingIndicator = () => {
-		return (
-			<View style={styles.activityOverlayStyle}>
-				<View style={styles.activityIndicatorContainer}>
-					<ActivityIndicator size="large" animating={this.state.webViewNotLoaded} color="green" />
-				</View>
-			</View>
-		);
-	};
+  createWebViewRef = webview => {
+    this.webview = webview;
+  };
 
-	onError = (error) => {
-		Alert.alert('WebView onError', error, [ { text: 'OK', onPress: () => console.log('OK Pressed') } ]);
-	};
+  handleMessage = event => {
+    console.log('WebViewQuillViewer handleMessage');
+  };
 
-	renderError = (error) => {
-		Alert.alert('WebView renderError', error, [ { text: 'OK', onPress: () => console.log('OK Pressed') } ]);
-	};
+  render = () => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#ffebba'
+        }}
+      >
+        {renderIf(this.state.webViewFilesNotAvailable)(
+          <View style={styles.activityOverlayStyle}>
+            <View style={styles.activityIndicatorContainer}>
+              <ActivityIndicator
+                size="large"
+                animating={this.state.webViewFilesNotAvailable}
+                color="blue"
+              />
+            </View>
+          </View>
+        )}
 
-	render = () => {
-		return (
-			<View
-				style={{
-					flex: 1,
-					backgroundColor: '#ffebba'
-				}}
-			>
-				<WebView
-					style={{
-						...StyleSheet.absoluteFillObject,
-						backgroundColor: '#ffebba',
-						padding: 10
-					}}
-					ref={this.createWebViewRef}
-					source={reactHtml}
-					onLoadEnd={this.webViewLoaded}
-					onMessage={this.handleMessage}
-					startInLoadingState={true}
-					renderLoading={this.showLoadingIndicator}
-					renderError={this.renderError}
-          onError={this.onError}
-          scalesPageToFit ={false}
-				/>
-			</View>
-		);
-	};
+        {renderIf(!this.state.webViewFilesNotAvailable)(
+          <WebView
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              backgroundColor: '#ffebba',
+              padding: 10
+            }}
+            ref={this.createWebViewRef}
+            source={{ uri: INDEX_FILE_PATH }}
+            onLoadEnd={this.webViewLoaded}
+            onMessage={this.handleMessage}
+          />
+        )}
+        {renderIf(
+          this.state.webViewNotLoaded && !this.state.webViewFilesNotAvailable
+        )(
+          <View style={styles.activityOverlayStyle}>
+            <View style={styles.activityIndicatorContainer}>
+              <ActivityIndicator
+                size="large"
+                animating={this.state.webViewNotLoaded}
+                color="orange"
+              />
+            </View>
+          </View>
+        )}
+      </View>
+    );
+  };
 }
 
 WebViewQuillViewer.propTypes = {
-	contentToDisplay: PropTypes.object
+  contentToDisplay: PropTypes.object
 };
 
 const styles = StyleSheet.create({
-	activityOverlayStyle: {
-		...StyleSheet.absoluteFillObject,
-		marginHorizontal: 20,
-		marginVertical: 60,
-		display: 'flex',
-		justifyContent: 'center',
-		alignContent: 'center',
-		borderRadius: 5
-	},
-	activityIndicatorContainer: {
-		backgroundColor: 'white',
-		padding: 10,
-		borderRadius: 50,
-		alignSelf: 'center',
-		shadowColor: '#000000',
-		shadowOffset: {
-			width: 0,
-			height: 3
-		},
-		shadowRadius: 5,
-		shadowOpacity: 1.0
-	}
+  activityOverlayStyle: {
+    ...StyleSheet.absoluteFillObject,
+    marginHorizontal: 20,
+    marginVertical: 60,
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    borderRadius: 5
+  },
+  activityIndicatorContainer: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50,
+    alignSelf: 'center',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 5,
+    shadowOpacity: 1.0
+  }
 });
