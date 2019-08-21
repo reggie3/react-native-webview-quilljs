466b0117c6734ea72259b4bf0ef31864b78aa7a8
hellomjx <2312951425@qq.com>
2019-08-22 01:11:49 +0800

    删除无用的文件

--- web/componentViewer.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom";
-import ReactQuillViewer from './reactQuillViewer';
-import React from "react";
-
-ReactDOM.render(<ReactQuillViewer />, document.getElementById("root"));
