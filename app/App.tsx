b7703b891fa4cd237a060885a40a9b46c2f23b92
Reginald Johnson <reginald.johnson@gmail.com>
2019-12-07 21:53:33 -0500

    update

---
+++ app/App.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Button,
+  Platform,
+  ActivityIndicator
+} from "react-native";
+import { Container, Header, Body, Title } from "native-base";
+import WebViewQuill from "react-native-webview-quilljs";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { DeltaOperation } from "quill";
+
+interface State {
+  content: DeltaOperation[];
+  editorHeight: number;
+  viewerHeight: number;
+  editorMessageDelta: any[];
+  isLoadingComplete: boolean;
+  hasLoadingStarted: boolean;
+  viewerMessageDelta: any[];
+}
+
+export default class App extends React.Component<null, State> {
+  webViewQuillEditor: any;
+  webViewQuillViewer: any;
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorMessageDelta: [
+        { insert: "Hello World" },
+        { insert: "!", attributes: { bold: true } }
+      ],
+      hasLoadingStarted: null,
+      isLoadingComplete: null,
+      viewerMessageDelta: {
+        ops: [
+          { insert: "Gandalf", attributes: { bold: true } },
+          { insert: " the " },
+          { insert: "Grey", attributes: { color: "#ccc" } }
+        ]
+      },
+      editorHeight: 0,
+      viewerHeight: 0
+    };
+  }
+
+  componentDidMount = () => {
+    Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    }).then(res => {
+      this.setState({ isLoadingComplete: true });
+    });
+    this.setState({ hasLoadingStarted: true });
+  };
+
+  onMessageReceived = (message: WebViewQuillJSMessage) => {
+    const { instruction, payload } = message;
+    if (payload.delta) {
+      this.setState({ content: payload.delta });
+    }
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete) {
+      return <ActivityIndicator />;
+    } else {
+      return (
+        <Container>
+          <Header
+            style={{
+              paddingTop: Platform.OS === "android" ? 30 : 10,
+              paddingBottom: 10
+            }}
+          >
+            <Body>
+              <Title>React Native Webview QuillJS V2 Demo</Title>
+            </Body>
+          </Header>
+          <View style={{ flex: 1, backgroundColor: "aliceblue", padding: 5 }}>
+            <View style={{ flex: 1, padding: 10, marginBottom: 15 }}>
+              <WebViewQuill
+                backgroundColor={"#FAEBD7"}
+                contentToDisplay={this.state.editorMessageDelta}
+                doShowDebugMessages={false}
+                doShowQuillComponentDebugMessages={false}
+                height={this.state.editorHeight}
+                onMessageReceived={this.onMessageReceived}
+                ref={component => (this.webViewQuillEditor = component)}
+              />
+            </View>
+            <View style={{ flex: 1 }} onLayout={this.onLayoutEditor}>
+              <WebViewQuill
+                backgroundColor={"#fffbea"}
+                defaultValue={this.state.viewerMessageDelta}
+                doShowDebugMessages={false}
+                doShowQuillComponentDebugMessages={false}
+                content={this.state.content}
+                height={this.state.viewerHeight}
+                isReadOnly
+                ref={component => (this.webViewQuillEditor = component)}
+              />
+            </View>
+          </View>
+        </Container>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "#ccccff",
+    display: "flex"
+  },
+  statusBar: {
+    height: 48,
+    backgroundColor: "#9be1ff"
+  }
+});
