fe9d1ab49931296b85d57ab16c9b3aa4a91a76e2
Reginald Johnson <reginald.johnson@gmail.com>
2019-12-31 15:35:36 -0500

    'update'

---
+++ html/build/precache-manifest.1623f385189e83a45b10fcafa9cdb815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2c58bbbff1f6650b9e3fcd8d6882b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3fff47053a098330b1b",
+    "url": "/static/css/2.4b3ccb6c.chunk.css"
+  },
+  {
+    "revision": "1390b34c51e0cbf1381a",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "c3fff47053a098330b1b",
+    "url": "/static/js/2.cf6de476.chunk.js"
+  },
+  {
+    "revision": "1390b34c51e0cbf1381a",
+    "url": "/static/js/main.4ad91d18.chunk.js"
+  },
+  {
+    "revision": "eecd9680c013c9396f32",
+    "url": "/static/js/runtime-main.4d715938.js"
+  }
+]);
