6582bb30cd0f511f00bb1a56ac07b28fa8028d75
Reginald Johnson <reginald.johnson@gmail.com>
2020-01-15 19:17:47 -0500

    update

---
+++ app/gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require("gulp");
+var clean = require("gulp-clean");
+
+gulp.task("clean", function() {
+  return gulp
+    .src("node_modules", { allowEmpty: true, read: false })
+    .pipe(clean());
+});
--- html/src/index.js
+++ html/src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import QuillComponent from './QuillComponent'
-// import QuillEditorComponent from './QuillEditorComponent';
-// import QuillViewerComponent from './QuillViewerComponent';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<QuillComponent />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-/* ReactDOM.render(
-    <div style={{ height: "500px", backgroundColor: "lightblue" }}>
-      <div style={{ flex: 1, backgroundColor: 'goldenrod' }}>
-        <QuillEditorComponent />
-      </div>
-      <div style={{ flex: 1, backgroundColor: 'lightsalmon' }}>
-        <QuillViewerComponent />
-      </div>
-    </div>,
-    document.getElementById("root")
-  ); */
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
